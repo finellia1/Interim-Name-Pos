ed3a52d018add6b882a99b63c3cb7cc744d18373
Doublefs <amfinelli2001@gmail.com>
2022-02-27 16:25:11 -0500

    Push Inventory System
    Inventory system

---
+++ js/inventory.js
@@ -0,0 +1,20 @@
+function editPane(){
+    document.getElementById('editPopup').style.visibility = 'visible';
+}
+
+function exitPane(ID){
+    document.getElementById(ID).style.visibility = 'hidden';
+}
+
+function deleteItem(){
+    console.log("Delete item");
+}
+
+function addItem(){
+    document.getElementById('addPopup').style.visibility = 'visible';
+}
+
+function searchItem(){
+    document.getElementById('searchPopup').style.visibility = 'visible';
+    console.log("asdassad");
+}
