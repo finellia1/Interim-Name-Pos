1a31aac139ab6bf0faacc4157306be9f047b480e
taimurtawil <92693639+taimurtawil@users.noreply.github.com>
2022-03-08 00:41:28 -0500

    Most Recent Working On Local

--- calendarPage.php
+++ calendarPage.php
@@ -1,8 +1,11 @@
 <!DOCTYPE html>
-<html>
-
+<html lang="en">
 <head>
-    <link rel="stylesheet" href="navBar.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>POS</title>
+
+    <link rel="stylesheet" href="calendarPage.css">
     <script>
             let img = document.querySelector('img');
             let start = img.src;
@@ -59,19 +62,57 @@
                     
                 </div>
             </div>
-            <div class = "middlePanel">
-                <h1> Middle Panel</h1>
-                <p>
-                    lipsum...
-                </p>
+            <div class = "middlePanel"> 
+                    <div id="container">
+                        <div id="header">
+                            <div id="monthDisplay"></div>
+                            <div>
+                            <button id="backButton">Back</button>
+                            <button id="nextButton">Next</button>
+                            </div>
+                        </div>
+
+                        <div id="weekdays">
+                            <div>Sunday</div>
+                            <div>Monday</div>
+                            <div>Tuesday</div>
+                            <div>Wednesday</div>
+                            <div>Thursday</div>
+                            <div>Friday</div>
+                            <div>Saturday</div>
+                        </div>
+
+                        <div id="calendar"></div>
+                        </div>
+
+                        <div id="newEventModal">
+                        <h2>New Event</h2>
+
+                        <input id="eventTitleInput" placeholder="Event Title"/>
+                        <textarea id="eventDescriptionInput" placeholder="Event Description" ></textarea>
+
+                        <button id="saveButton">Save</button>
+                        <button id="cancelButton">Cancel</button>
+                        </div>
+
+                        <div id="deleteEventModal">
+                        <h2>Event</h2>
+
+                        <p id="eventText"></p>
+
+                        <button id="deleteButton">Delete</button>
+                        <button id="closeButton">Close</button>
+                        </div>
+
+                        <div id="modalBackDrop"></div>
+                        <script src="calendarScript.js"></script>
             </div>
             <div class = "rightPanel">
                 <h1> Right Panel </h1>
-                <p>
-                    lipsum...
-                </p>
+                <div id="eventInfo"></div>
             </div>
         </main>
     </div>
 
 </body>     
+</html>
