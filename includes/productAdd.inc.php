d833921b726b41bc8f4f342d6db251e53add1d7c
Doublefs02 <54219740+Doublefs02@users.noreply.github.com>
2022-05-05 15:04:32 -0400

    Revert to previous add controller
    Using previous version of add controller due to stability issues with scraping input for rogue quotations. Will use main controller for final submissions.

--- includes/productAdd.inc.php
+++ includes/productAdd.inc.php
@@ -10,6 +10,42 @@
     $model = $_POST["model"];
     $qty_unit = $_POST["qty_unit"];
     $qty_in_stock = $_POST["qty_in_stock"];
+    if($is_promotional == true){
+        $is_promotional = 1;
+    }else{
+        $is_promotional = 0;
+    }    
+	$reg_price = $_POST["reg_price"];
+    $discounted_price= $_POST["discounted_price"];
+    $num_rented = $_POST["num_rented"];
+    $num_broken = $_POST["num_broken"];
+
+    
+    // instantiate signupContr class
+    include "../classes/dbh.classes.php";
+    include "../classes/productAdd.classes.php";
+    include "../classes/productAdd-contr.classes.php";
+    $addProduct = new productAddContr($product_ID,$vendor, $product_name, $product_description, $product_type, $make, $model, $qty_unit, $qty_in_stock,$is_promotional,$reg_price,$discounted_price,$num_rented,$num_broken);
+    // running error handlers and user signup
+    $addProduct-> addProduct($product_ID, $vendor,$product_name, $product_description, $product_type, $make, $model, $qty_unit, $qty_in_stock,$is_promotional,$reg_price,$discounted_price,$num_rented,$num_broken);
+    // going back to front page
+    header("location: ../inventory.php?error=ran");
+// }
+?>
+
+
+<?php
+// if(isset($_POST["submit"])) 
+// {
+    $product_ID = "add";
+    $vendor = $_POST["vendorInput"];
+    $product_name = $_POST["product_name"];
+    $product_description = $_POST["product_description"];
+    $product_type = $_POST["product_type"];
+    $make= $_POST["make"];
+    $model = $_POST["model"];
+    $qty_unit = $_POST["qty_unit"];
+    $qty_in_stock = $_POST["qty_in_stock"];
     $is_promotional = isset($_POST["isPromotional"]); //needs to be fixed
     if($is_promotional == true){
         $is_promotional = 1;
@@ -32,7 +68,7 @@
 
     //Clean name input
     $c_name = $product_name;
-    if(str_contains($product_name, '"')){
+    if(str_contains($c_name, '"')){
         $c_name = str_replace('"', "“", $product_name);
     }
     if(str_contains($c_name, "'")){
@@ -79,9 +115,9 @@
     include "../classes/dbh.classes.php";
     include "../classes/productAdd.classes.php";
     include "../classes/productAdd-contr.classes.php";
-    $addProduct = new productAddContr($product_ID,$vendor, $c_name, $c_description, $c_type, $c_make, $c_model, $c_qty_unit, $qty_in_stock,$is_promotional,$reg_price,$discounted_price,$num_rented,$num_broken);
+    $addProduct = new productAddContr($product_ID, $vendor,$product_name, $product_description, $product_type, $make, $model, $qty_unit, $qty_in_stock,$is_promotional,$reg_price,$discounted_price,$num_rented,$num_broken);
     // running error handlers and user signup
-    $addProduct-> addProduct($product_ID, $vendor,$c_name, $c_description, $c_type, $c_make, $c_model, $c_qty_unit, $qty_in_stock,$is_promotional,$reg_price,$discounted_price,$num_rented,$num_broken);
+    $addProduct-> addProduct($product_ID, $vendor,$product_name, $product_description, $product_type, $make, $model, $qty_unit, $qty_in_stock,$is_promotional,$reg_price,$discounted_price,$num_rented,$num_broken);
     // going back to front page
     header("location: ../inventory.php?error=ran");
 // }
